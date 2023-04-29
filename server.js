5c3a4bea8b2587408cc2aa2633bf0d4210f1e858
jkrieger6 <jkrieger11@gmail.com>
2023-04-29 10:27:12 -0700

    added POST method to add new notes

--- server.js
+++ server.js
@@ -19,6 +19,44 @@ app.get("/public/notes", (req, res) => {
     console.log("GET request received")
 });
 
+// POST request to add notes
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+        // Convert data into a string so it can be saved
+        const reviewString =JSON.stringify(newNote);
+        // Add string to a file
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(newNote);
+                fs.writeFile(".db/db.json",
+                JSON.stringify(parsedNotes, null,),
+                (writeErr) =>
+                writeErr
+                ? console.error(writeErr)
+                : console.info("Successfully updated notes!")
+                );
+            }
+    });
+    const response = {
+        status: "success",
+        body: newNote,
+    };
+    console.log(response);
+    res.json(response);
+} else {
+    res.json("Error in creating note");
+}
+});
+
 // Listener to connect to port
 app.listen(PORT, () =>
 console.log(`App listening att http://localhost:${PORT} 🚀`)
