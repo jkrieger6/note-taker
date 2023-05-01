f237744643b66ceae189a6ff6338c7c594efc481
jkrieger6 <jkrieger11@gmail.com>
2023-05-01 08:39:51 -0700

    added paths to retrieve notes and tried to get the ability to add notes

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 const fs = require('fs');
 const path = require('path');
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Listener to connect to server
-app.get("/", (req, res) => 
-    res.sendFile(path.join(__dirname, "/public/index.html")));
-
-// GET request to retrieve notes
-app.get("/public/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-    console.log("GET request received")
+// Listener to connect to server and retrieve notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
+
+
+// API routes
+app.get('/api/notes', (req, res) => {
+    const notes =JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(notes);
+})
 // POST request to add notes
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note`);
