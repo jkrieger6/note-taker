3184a44d65738689d1bc8b2f94b0f72a92a3d824
jkrieger6 <jkrieger11@gmail.com>
2023-05-05 08:56:54 -0700

    routed notes to index

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 // const uuid = require('/develop/helpers/uuid.js');
+const notes = require('./routes/notes.js');
 
 
 const fs = require('fs');
@@ -9,50 +10,48 @@ const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('develop/public'));
+
 
 // GET route to connect to server and retrieve notes
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "develop/public/index.html"))
-});
 
-app.get("/api/notes", (req, res) => {
-    res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//     res.json(notes);
+// });
 
 // GET route to retrieve notes from db.json file and return all saved notes as JSON
-app.get("/api/notes", (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    fs.readFile("./db/db.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
-});
+// app.get("/api/notes", (req, res) => {
+//     console.info(`${req.method} request received for notes`);
+//     fs.readFile("./db/db.json", (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             res.json(JSON.parse(data));
+//         }
+//     });
+// });
 // POST request to add notes and give them a unique id using uuid node package
-app.post("/api/notes", (req, res) => {
-    // Logs that the POST request was received
-    console.info(`${req.method} request received to add a note`);
-    const { title, text } = req.body;
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            id: uuid.v4(),
-        };
-        notes.push(newNote);
-        const response = {
-            status: "success",
-            body: newNote,
-        };
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json("Error in posting note");
-    }
-});
+// app.post("/api/notes", (req, res) => {
+//     // Logs that the POST request was received
+//     console.info(`${req.method} request received to add a note`);
+//     const { title, text } = req.body;
+//     if (req.body) {
+//         const newNote = {
+//             title,
+//             text,
+//             id: uuid.v4(),
+//         };
+//         notes.push(newNote);
+//         const response = {
+//             status: "success",
+//             body: newNote,
+//         };
+//         console.log(response);
+//         res.status(201).json(response);
+//     } else {
+//         res.status(500).json("Error in posting note");
+//     }
+// });
         // Convert data into a string so it can be saved
         // const reviewString =JSON.stringify(newNote);
         // Add string to a file
@@ -72,30 +71,16 @@ app.post("/api/notes", (req, res) => {
     //         }
     // });
 
-// DELETE request to delete notes that have been aded(bonus points)
-app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id;
-    fs.readFile("./db/db.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedNotes = JSON.parse(data);
-            const newNotes = parsedNotes.filter((note) => note.id !== id);
-            fs.writeFile("./db/db.json",
-            JSON.stringify(newNotes),
-            (writeErr) =>
-            writeErr
-            ? console.error(writeErr)
-            : console.info("Successfully deleted note!")
-            );
-        }
-    });
-    const response = {
-        status: "success",
-        body: id,
-    };
-    console.log(response);
-    res.json(response);
+// 
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "develop/public/notes.html"))
+});
+
+app.use('/api', notes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "develop/public/index.html"))
 });
 
 // Listener to connect to port
