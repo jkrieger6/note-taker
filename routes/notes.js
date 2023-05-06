5902cc834e3a3206e46366d659a88b9bbca357f3
jkrieger6 <jkrieger11@gmail.com>
2023-05-06 11:34:23 -0700

    reformatted with prettier

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const notes = require("express").Router();
 const uuid = require("uuid");
-const util = require('util');
-const fs = require('fs');
+const util = require("util");
+const fs = require("fs");
 
 // Function to write notes to db.json file
 const writeToFile = (destination, content) =>
@@ -11,74 +11,77 @@ const writeToFile = (destination, content) =>
 
 // Function to read and append notes to db.json file
 const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 // Function to read from db.json file
-  const readFromFile = util.promisify(fs.readFile);
+const readFromFile = util.promisify(fs.readFile);
 
 // GET route for retrieving all the notes from db.json file
 notes.get("/notes", (req, res) => {
-    readFromFile("./develop/db/db.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("./develop/db/db.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST route for adding notes and giving them a unique id using uuid node package
 notes.post("/notes", (req, res) => {
-    // Logs that the POST request was received
-    console.info(`${req.method} request received to add a note`);
-    const { title, text } = req.body;
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            id: uuid.v4(),
-        };
+  // Logs that the POST request was received
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid.v4(),
+    };
 
-        readAndAppend(newNote, "./develop/db/db.json");
+    readAndAppend(newNote, "./develop/db/db.json");
 
-        const response = {
-            status: "success",
-            body: newNote,
-        };
-        console.log(response);
-        res.status(201).json(newNote);
-    } else {
-        res.status(500).json("Error in posting note");
-    }
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(newNote);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
 });
 
 // DELETE request to delete notes that have been added
 notes.delete("/notes/:id", (req, res) => {
-    const id = req.params.id;
-    fs.readFile("./develop/db/db.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedNotes = JSON.parse(data);
-            const newNotes = parsedNotes.filter((note) => note.id !== id);
-            fs.writeFile("./develop/db/db.json",
-            JSON.stringify(newNotes),
-            (writeErr) =>
-            writeErr
+  const id = req.params.id;
+  fs.readFile("./develop/db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      const newNotes = parsedNotes.filter((note) => note.id !== id);
+      fs.writeFile(
+        "./develop/db/db.json",
+        JSON.stringify(newNotes),
+        (writeErr) =>
+          writeErr
             ? console.error(writeErr)
             : console.info("Successfully deleted note!")
-            );
-        }
-    });
-    const response = {
-        status: "success",
-        body: id,
-    };
-    console.log(response);
-    res.json(response);
+      );
+    }
+  });
+  const response = {
+    status: "success",
+    body: id,
+  };
+  console.log(response);
+  res.json(response);
 });
 
-  module.exports = notes;
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const notes = require('./routes/notes.js');
+const notes = require("./routes/notes.js");
 
-
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('develop/public'));
+app.use(express.static("develop/public"));
 
 // GET route to retrieve notes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "develop/public/notes.html"))
+  res.sendFile(path.join(__dirname, "develop/public/notes.html"));
 });
 
-app.use('/api', notes);
+app.use("/api", notes);
 
 // GET route to return all other routes to the home page
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "develop/public/index.html"))
+  res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
 // Listener to connect to port
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
